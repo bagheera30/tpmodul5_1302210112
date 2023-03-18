18c75833e882262435596ace95afb8ef3186b4fe
bagheera30 <rizkialfian30103@gmail.com>
2023-03-18 22:06:32 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302210112/Program.cs
+++ tpmodul5_1302210112/Program.cs
@@ -6,8 +6,22 @@
             Console.WriteLine("Hallo user " + inputuser);
         }
     }
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
     private static void Main(string[] args)
     {
         HaloGeneric.sapaUser<string>("Alfian");
+        DataGeneric<string> nim = new DataGeneric<string>("1302210112");
+        nim.PrintData();
     }
 }
